2ebf92444d2d094587719ce5b7a22375c015a1c6
oliver.vilchis.contractor <oliver.vilchis.contractor@bbva.com>
2022-10-09 18:08:53 -0500

    version inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Author: Oliver Vilchis
+#Programa que se encargara de presentar las funciones de un cajero automatico, donde se podran realizar tres acciones "Consultar saldo,retirar saldo y visualizar el historico de acciones"
+
+def main():
+    """Metodo que inicializa el programa"""
+    print("hola mundo!!")
+
+if __name__ == '__main__':
+    main()
